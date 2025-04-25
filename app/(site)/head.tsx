985517ef317e15e6ac48f914210c65195921769b
Himayun <121334727+Himayun7@users.noreply.github.com>
2025-04-25 12:30:15 +0530

    head.tsx

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport"/>
       <meta name="description" content="Built with voxwave ai"/>
       <link rel="icon" href="/images/favicon.ico"/>
-      <Script src="/voxwave.js" strategy="beforeInteractive" />
+      
 
     </>
   );
